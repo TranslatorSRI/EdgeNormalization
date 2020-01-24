8ebd0102e104067233e9e8b4d304c9c3c378def8
YaphetKG <kebedey@renci.org>
2020-01-24 14:06:25 -0500

    'Environmentalizing' BL lookup server

--- src/biolink_client.py
+++ src/biolink_client.py
@@ -1,12 +1,13 @@
 from src.util import Text
 import requests
+import os
 
 class biolink:
     """This class is merely a placeholder.  Currently it's implemented
     by pulling in the biolink yaml.  But it's going to be re-implemented as a client
     on a biolink model service."""
     def __init__(self):
-        self.url_base = 'http://robokop.renci.org:8144'
+        self.url_base = os.environ.get('BL_HOST', 'http://robokop.renci.org:8144')
 
     def get_label_by_iri(self,curie):
         url = f'{self.url_base}/uri_lookup/{curie}'
@@ -17,7 +18,7 @@ class biolink:
         url = f'{self.url_base}/bl/{Text.snakify(concept)}'
         result = requests.get(url).json()
         if result is not None:
-            return result['slot_uri']
+            return result.get('slot_uri', Text.snakify(concept))
         return None
 
 
