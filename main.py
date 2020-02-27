ffff9c5544eacb8cbfd702fd1174e08d1a5999ee
Phil Owen <phillipsowen@outlook.com>
2020-02-27 12:16:37 -0500

    fix typo in arg parser

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 import argparse
 from src.server import app
 
-parser = argparse.ArgumentParser(description='Start BL lookup server.')
+parser = argparse.ArgumentParser(description='Start Edge Normalization server.')
 parser.add_argument('--host', default='0.0.0.0', type=str)
 parser.add_argument('--port', default=8145, type=int)
 
