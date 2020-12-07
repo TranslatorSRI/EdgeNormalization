65a642a952fb2b71f7b8e834d74b4e3acb053020
Chris Bizon <bizon@rciadmins-mbp.lan>
2020-12-07 16:54:30 -0500

    Updated EdgeNormalizer to deal with change in mapping (1.4.0)

--- tests/test_resolver.py
+++ tests/test_resolver.py
@@ -2,17 +2,17 @@ import pytest
 from src.resolver import EdgeNormalizer
 
 def test_RO_exact():
-    '''If we have an RO that is a slot uri, return an edge with that identfier'''
+    '''If we have an RO that is an exact match, return an edge with that identfier'''
     r = EdgeNormalizer()
-    edge = r.resolve_curie('RO:0002410')
+    edge = r.resolve_curie('RO:0002506')
     assert edge.identifier == 'biolink:causes'
 
 def test_exact_slot_URI_non_RO():
     '''If we have a curie that is not a RO, but is a slot uri, return it as an edge identifier'''
     r = EdgeNormalizer()
-    edge = r.resolve_curie('WD:P2293')
-    assert edge.identifier == 'biolink:gene_associated_with_condition'
-    assert edge.label == 'gene_associated_with_condition'
+    edge = r.resolve_curie('WIKIDATA_PROPERTY:P2293')
+    assert edge.identifier == 'biolink:genetic_association'
+    assert edge.label == 'genetic association'
 
 def test_exact_mapping():
     '''If we have a curie that is a direct mapping, but not a slot uri, return the corresponding slot uri as an edge identifier'''
@@ -40,6 +40,6 @@ def test_RO_bad():
     '''RO isn't single rooted.  So it's easy to get to the follow our plan and not get anywhere.  In that case,
     we want to hit related_to by fiat.'''
     r = EdgeNormalizer()
-    edge = r.resolve_curie('RO:0002004')
-    assert edge.label == 'related_to'
+    edge = r.resolve_curie('RO:0002214') #has prototype
+    assert edge.label == 'related to'
     assert edge.identifier == 'biolink:related_to'
